616e80eb7184a5a26780842220fbc308650fe00a
huhaoda <huhaoda@shouqianba.com>
2023-07-04 19:22:59 +0800

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx|.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      titel: 'react app',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+};
